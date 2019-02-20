86fd6690ba3b24deb534e0deee0a898c67874031
TheoIsambourg <theo.isambourg@gmail.com>
2019-02-20 21:40:41 +0100

    resto: get list of restaurants

--- resto.js
+++ resto.js
@@ -4,27 +4,55 @@ var fs = require('fs');
 const fetch = require("node-fetch");
 
 
-scrappingRestos()
+const URLpage = "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin"
 
-async function scrappingRestos(){
+const AllURLs = scrapURLs(URLpage)  //1st : we scrap the URLs of the 35 pages of retaurants with at minimum 1 star
 
+scrappingRestos(AllURLs)//2nd : we scrap the names of the restaurants
+
+
+function scrapURLs(URL){
+
+  var listOfURLs = []
+
+  listOfURLs[0] = URL
+
+  for(var j = 1; j < 35; j++) 
+  {
+    listOfURLs[j] =  URL  +  '/page-'+  (j+1).toString()
+  }
+
+  return listOfURLs;
+
+}
+
+
+
+
+
+async function scrappingRestos(listOfURLs){
     var listOfRestaurants = [];
-    var res = await fetch("https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin")
-    res = await res.text()
-    var $ = cheerio.load(res);
 
-    $('div .poi_card-description').each(function(i) //context
-    {   
-        $(this).find("div .poi_card-display-title").each(function(k)
-        {
-            var name= $(this).text().trim();
-            listOfRestaurants.push({ "name": name})
-        })
+    for (var j = 0; j < listOfURLs.length; j ++){
         
-    });
+        var res = await fetch(listOfURLs[j])
+        res = await res.text()
+        var $ = cheerio.load(res);
+
+        $('div .poi_card-description').each(function(i) //context
+        {   
+            $(this).find("div .poi_card-display-title").each(function(k)
+            {
+                var name= $(this).text().trim();
+                listOfRestaurants.push({ "name": name})
+            })
+            
+        });
     
 
    
-   fs.writeFileSync("listOfRestaurantsEtoiles.json", JSON.stringify(listOfRestaurants, null, 2));
+    }
+fs.writeFileSync("listOfRestaurantsEtoiles.json", JSON.stringify(listOfRestaurants, null, 2));
+
 }
 
